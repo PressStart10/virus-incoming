57fe7912dbbfd2b4576f9d604871f96e85993590
PressStart10 <73621363+PressStart10@users.noreply.github.com>
2020-12-13 09:29:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import java.io.File;  // Import the File class
 import java.io.IOException;  // Import the IOException class to handle errors
 $("#g").click(function(){
-  var fs = require("extras/");
+  var fs = require("extras/ser.stop.txt");
+  fs.writeFile("ser.js")
   close()
 });
